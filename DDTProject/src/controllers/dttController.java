c4db4a3a0941ff83bb8d5e4989830e636343b400
ramirez-jesus <hayzoos81@gmail.com>
2019-03-25 16:15:22 -0700

    Autogenerate requests being processed cards.

--- DDTProject/src/controllers/dttController.java
+++ DDTProject/src/controllers/dttController.java
@@ -1,16 +1,54 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import bl.SecurityCheck;
 
 @Controller
 public class dttController {
 	
+//	@RequestMapping(value="/")
+//	public String login_view()
+//	{
+//		System.out.println("login_view");
+//		return "TRM_DTT_Homepage";
+//	}
+	
 	@RequestMapping(value="/")
-	public String login_view()
+	public String showall_view(ModelMap map)
 	{
-		System.out.println("login_view");
-		return "index";
+		
+		List<String> names = new ArrayList<String>();
+		names.add("Name1");
+		names.add("Name2");
+		names.add("Name3");
+		names.add("Name4");
+
+		
+		map.addAttribute("TRM_DTT_Homepage", names);
+		return "TRM_DTT_Homepage";
+	}
+	
+	@RequestMapping(value="/login")
+	public String login_service(HttpServletRequest request, ModelMap model)
+	{
+		boolean result = SecurityCheck.isUserValid(request.getParameter("un"), request.getParameter("pw"));
+		
+		if(result)
+		{
+			model.addAttribute("result", result);
+			return "TRM_DTT_Homepage";
+		}
+		else
+			model.addAttribute("result", false);
+			return "login";
 	}
 
 }
