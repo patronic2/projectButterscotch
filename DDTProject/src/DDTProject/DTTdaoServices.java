3ca9ddadc147f00921151f1c78321b7db01dd38a
Patrick <patronic64@gmail.com>
2019-03-27 11:02:51 -0700

    Update DTTdaoServices.java
    Updated SQL so only in progress training cards are showed

--- DDTProject/src/DDTProject/DTTdaoServices.java
+++ DDTProject/src/DDTProject/DTTdaoServices.java
@@ -11,7 +11,7 @@ public class DTTdaoServices {
 	
 	public List<DTTProcessingCard> getRequestsProcessing()
 	{
-		String query = "SELECT tr.training_request_id, e.last_name, e.first_name, tr.request_training_type, tr.request_training_module, tr.request_training_module_scope, tr.request_start_date, tr.request_end_date, tr.request_location, tr.request_approx_participant FROM training_request tr INNER JOIN employee e ON tr.requester_id = e.employee_id";
+		String query = "SELECT tr.training_request_id, e.last_name, e.first_name, tr.request_training_type, tr.request_training_module, tr.request_training_module_scope, tr.request_start_date, tr.request_end_date, tr.request_location, tr.request_approx_participant FROM training_request tr INNER JOIN employee e ON tr.requester_id = e.employee_id INNER JOIN training_management_status tms ON tms.training_request_id = tr.training_request_id where tms.status > 100";
 		return temp.query(query, new DTTProcessingCardMapper());
 	}
 	
