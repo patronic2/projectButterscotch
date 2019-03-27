9e4cb6d34fb6067a852bfd5666ab2f1e83a08952
Pablo De La Cruz <syntel@PHX440G3-2815ZZ.syntelorg.com>
2019-03-27 11:10:54 -0700

    accordion data from db

---
+++ DDTProject/src/DDTProject/TrainingSchedule.java
@@ -0,0 +1,101 @@
+package DDTProject;
+
+import java.net.URL;
+import java.sql.Timestamp;
+
+public class TrainingSchedule {
+	private int trainingScheduleId;
+	private String trainingCity;
+	private String trainingState;
+	private String trainingCountry;
+	private int trainingZipcode;
+	private String trainingTimeZone;
+	private String trainingLocation;
+	private String trainingRoomNumber;
+	private String trainingStartDate;
+	private String trainingEndDate;
+	private String trainingBreakDown;
+	private String trainingUrl;
+	private String trainingPhone;
+	
+	public int getTrainingScheduleId() {
+		return trainingScheduleId;
+	}
+	public void setTrainingScheduleId(int trainingScheduleId) {
+		this.trainingScheduleId = trainingScheduleId;
+	}
+	public String getTrainingCity() {
+		return trainingCity;
+	}
+	public void setTrainingCity(String trainingCity) {
+		this.trainingCity = trainingCity;
+	}
+	public String getTrainingState() {
+		return trainingState;
+	}
+	public void setTrainingState(String trainingState) {
+		this.trainingState = trainingState;
+	}
+	public String getTrainingCountry() {
+		return trainingCountry;
+	}
+	public void setTrainingCountry(String trainingCountry) {
+		this.trainingCountry = trainingCountry;
+	}
+	public int getTrainingZipcode() {
+		return trainingZipcode;
+	}
+	public void setTrainingZipcode(int trainingZipcode) {
+		this.trainingZipcode = trainingZipcode;
+	}
+	public String getTrainingTimeZone() {
+		return trainingTimeZone;
+	}
+	public void setTrainingTimeZone(String trainingTimeZone) {
+		this.trainingTimeZone = trainingTimeZone;
+	}
+	public String getTrainingLocation() {
+		return trainingLocation;
+	}
+	public void setTrainingLocation(String trainingLocation) {
+		this.trainingLocation = trainingLocation;
+	}
+	public String getTrainingRoomNumber() {
+		return trainingRoomNumber;
+	}
+	public void setTrainingRoomNumber(String trainingRoomNumber) {
+		this.trainingRoomNumber = trainingRoomNumber;
+	}
+	public String getTrainingStartDate() {
+		return trainingStartDate;
+	}
+	public void setTrainingStartDate(String trainingStartDate) {
+		this.trainingStartDate = trainingStartDate;
+	}
+	public String getTrainingEndDate() {
+		return trainingEndDate;
+	}
+	public void setTrainingEndDate(String trainingEndDate) {
+		this.trainingEndDate = trainingEndDate;
+	}
+	public String getTrainingBreakDown() {
+		return trainingBreakDown;
+	}
+	public void setTrainingBreakDown(String trainingBreakDown) {
+		this.trainingBreakDown = trainingBreakDown;
+	}
+	public String getTrainingUrl() {
+		return trainingUrl;
+	}
+	public void setTrainingUrl(String trainingUrl) {
+		this.trainingUrl = trainingUrl;
+	}
+	public String getTrainingPhone() {
+		return trainingPhone;
+	}
+	public void setTrainingPhone(String trainingPhone) {
+		this.trainingPhone = trainingPhone;
+	}
+
+	
+}
